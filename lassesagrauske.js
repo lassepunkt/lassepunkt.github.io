44641ea4e639315c52482244de38e40491963ca0
lassepunkt <122087541+lassepunkt@users.noreply.github.com>
2024-09-01 12:38:56 +0200

    Dark/Lightmode

--- lassesagrauske.js
+++ lassesagrauske.js
@@ -1,3 +1,65 @@
+function changeMode() {
+    const mode = document.getElementById('mode')
+
+    const p = document.getElementsByTagName('p')
+    const h1 = document.getElementsByTagName('h1')
+    const h2 = document.getElementsByTagName('h2')
+    const button = document.getElementsByTagName('button')
+    const a = document.getElementsByTagName('a')
+
+    const textColorLight = '#121212'
+    const textColorDark = '#E2E2E2'
+    const backgroundColorLight = '#FFFFFF'
+    const backgroundColorDark = '#121212'
+
+    if (mode.innerHTML == 'Light') {
+        mode.innerHTML = 'Dark'
+        mode.style.color = textColorLight
+        mode.style.border = '1px solid ' + textColorLight
+        mode.style
+        document.getElementById('body').style.backgroundColor = backgroundColorLight
+        for (i = 0; i < p.length; i++) {
+            p[i].style.color = textColorLight
+        }
+        for (i = 0; i < h1.length; i++) {
+            h1[i].style.color = textColorLight
+        }
+        for (i = 0; i < h2.length; i++) {
+            h2[i].style.color = textColorLight
+        }
+        for (i = 0; i < button.length; i++) {
+            button[i].style.color = textColorLight
+            button[i].style.border = '1px solid ' + textColorLight
+            // button[i].onmouseover.style.color = backgroundColorLight
+        }
+        for (i = 0; i < a.length; i++) {
+            a[i].style.color = textColorLight
+        }
+    } else {
+        mode.innerHTML = 'Light'
+        mode.style.color = textColorDark
+        mode.style.border = '1px solid ' + textColorDark
+        document.getElementById('body').style.backgroundColor = backgroundColorDark
+        for (i = 0; i < p.length; i++) {
+            p[i].style.color = textColorDark
+        }
+        for (i = 0; i < h1.length; i++) {
+            h1[i].style.color = textColorDark
+        }
+        for (i = 0; i < h2.length; i++) {
+            h2[i].style.color = textColorDark
+        }
+        for (i = 0; i < button.length; i++) {
+            button[i].style.color = textColorDark
+            button[i].style.border = '1px solid ' + textColorDark
+        }
+        for (i = 0; i < a.length; i++) {
+            a[i].style.color = textColorDark
+        }
+
+    }
+}
+
 var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 options = {
     year: 'numeric',
